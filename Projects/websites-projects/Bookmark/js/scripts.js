a0846cf60f683558ab75096449522d6671b34063
Arthur Markiz <tutumarkiz26@gmail.com>
2024-08-08 21:37:28 -0300

    add project: Bookmark part-2

--- Projects/websites-projects/Bookmark/js/scripts.js
+++ Projects/websites-projects/Bookmark/js/scripts.js
@@ -1,5 +1,21 @@
 const tabs = document.querySelectorAll('.tab');
 const panels = document.querySelectorAll('.panel');
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+const logo = document.getElementById('logo');
+
+// Hamburguer Button Toggle
+const navToggle = () => {
+    btn.classList.toggle('open');
+    menu.classList.toggle('flex');
+    menu.classList.toggle('hidden');
+
+    if (menu.classList.contains('flex')) {
+        logo.setAttribute('src', './images/logo-bookmark-footer.svg');
+    } else {
+        logo.setAttribute('src', './images/logo-bookmark.svg');
+    }
+};
 
 const onTabClick = (e) => {
     // Deactivate all tabs
@@ -24,4 +40,8 @@ const onTabClick = (e) => {
 
 // Tabs menu event 
 tabs.forEach((tab) => tab.addEventListener('click', onTabClick));
+
+// Hamburguer Button event
+btn.addEventListener('click', navToggle);
+
 
